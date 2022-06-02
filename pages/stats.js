8cd40748daaa0aab280366556df78ba1beb1d2ec
Alex Yip <alex.yip@adhocteam.us>
2022-06-02 11:51:13 -0700

    Added Google Cloud Datastore query example

--- pages/stats.js
+++ pages/stats.js
@@ -1,7 +1,11 @@
+const {Datastore} = require('@google-cloud/datastore')
+const {getAuth} = require('../server/auth')
+
 function Stats(props) {
   return (
     <div>
-      {props.message} {props.userInfo.email}
+      <p>{props.message} {props.userInfo.email}</p>
+      <p>Document: {props.documentId} has been viewed {props.viewCount} time(s) by {props.email}</p>
     </div>
   )
 }
@@ -10,13 +14,50 @@ export async function getServerSideProps(context) {
   // Pass data to the page via props
   console.log(context.req.userInfo)
 
+  const result = await getDocumentById('1CEkEK7NzQ0oXKXZWSTsHfSeqnwohHwVgSjBqB2cOnPM')
+
+  console.log(result)
+
   return {
     props: {
-      data: [1, 2, 3],
       message: 'Hello, ',
-      userInfo: context.req.userInfo
+      userInfo: context.req.userInfo,
+      viewCount: result[0].viewCount,
+      documentId: result[0].documentId,
+      email: result[0].email
     }
   }
+}
+
+/* Support Functions */
+
+async function getDatastoreClient() {
+  const projectId = process.env.GCP_PROJECT_ID
+  if (!projectId) {
+    console.log('No GCP_PROJECT_ID provided! Will not connect to GCloud Datastore!')
+    return null
+  }
+
+  // because auth credentials may be passed in multiple ways, recycle pathway used by main auth logic
+  const {email, key} = await getAuth()
+
+  return new Datastore({
+    projectId,
+    credentials: {
+      client_email: email,
+      private_key: key
+    }
+  })
+}
+
+async function getDocumentById(documentId) {
+  const datastore = await getDatastoreClient()
+
+  const query = datastore.createQuery(['LibraryViewDoc']).filter('documentId', '=', documentId)
+
+  const result = await datastore.runQuery(query)
+
+  return result.length > 0 ? result[0] : null
 }
 
 export default Stats
