40489ae859aa768265cf1a9c0b2369d525970b08
Alex Yip <alex.yip@adhocteam.us>
2022-06-09 09:34:46 -0700

    Added additional stimulus set up

---
+++ scripts/index.ts
@@ -0,0 +1,9 @@
+import { Application } from "@hotwired/stimulus"
+
+// Import controller here
+// import HelloController from "./controllers/hello_controller"
+
+const application = Application.start()
+
+// Register controller here
+// application.register("hello", HelloController)
