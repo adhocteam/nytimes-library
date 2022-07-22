5a2b5506a228c4428a3fe3d1a4c69db86aa1a3d1
ahinh43 <alec4555@gmail.com>
2022-07-22 15:20:01 -0500

    Add sort order for version finder script

--- ops/gcloud_secrets_helper.sh
+++ ops/gcloud_secrets_helper.sh
@@ -9,9 +9,9 @@ version="$2"
 
 if [ -z "$version" ]; then
   # shellcheck disable=SC2086
-  version="$(gcloud secrets versions list $secret_name --filter=enabled --limit=1 --format=json | jq -r '.[].name' | sed 's/.*\///g')"
+  version="$(gcloud secrets versions list $secret_name --filter=enabled --sort-by='`' --limit=1 --format=json | jq -r '.[].name' | sed 's/.*\///g')"
 fi
 
-# Return the secret
+# Return the secret (note this does not know if the secret is base64 encoded. If it is encoded, you must pipe this scripts output to a base64 -d command)
 
 gcloud secrets versions access "$version" --secret="$secret_name"
