25654acfa5432f544e9ae46008ab45ca4c18c9ef
Nick Sayre <nick.sayre@adhocteam.us>
2022-08-01 17:37:10 -0500

    Replace overzealous JavaScript with simple link

--- scripts/index.ts
+++ scripts/index.ts
@@ -3,7 +3,6 @@ import { Application } from "@hotwired/stimulus"
 // Import controllers here
 import AttributionController from "./controllers/attribution_controller"
 import ChildrenListController from "./controllers/children_list_controller"
-import EditButtonController from "./controllers/edit_button_controller"
 import SearchController from "./controllers/search_controller"
 import UsefulnessController from './controllers/usefulness_controller'
 import UserToolsController from './controllers/user_tools_controller'
@@ -13,7 +12,6 @@ const application = Application.start()
 // Register controllers here
 application.register("attribution", AttributionController)
 application.register("children-list", ChildrenListController)
-application.register('edit-button', EditButtonController)
 application.register('search', SearchController)
 application.register('usefulness', UsefulnessController)
 application.register('user-tools', UserToolsController)
