6acc428afccc9fdc54e17ed8f26d75ad5392265a
ahinh43 <alec4555@gmail.com>
2022-08-04 09:18:39 -0500

    Add needed IAM roles to the GHA account

--- ops/terraform/modules/service_account/main.tf
+++ ops/terraform/modules/service_account/main.tf
@@ -12,15 +12,40 @@ resource "google_service_account" "gha" {
   description  = "For use with Github actions"
 }
 
-# Grant the GHA service account the app engine deployer role
 
-resource "google_project_iam_member" "gha" {
+# Grant the GHA service account needed permissions to deploy to GAE
+# This doc was referenced when determmining what roles are needed
+# https://cloud.google.com/appengine/docs/legacy/standard/python/roles
+
+resource "google_project_iam_member" "gha_appengine" {
   count   = var.create_gha ? 1 : 0
   project = var.project_id
   role    = "roles/appengine.deployer"
   member  = "serviceAccount:${google_service_account.gha[0].email}"
 }
 
+# Grants GHA the ability to admin GCS buckets
+resource "google_project_iam_member" "gha_gcs" {
+  count   = var.create_gha ? 1 : 0
+  project = var.project_id
+  role    = "roles/storage.objectAdmin"
+  member  = "serviceAccount:${google_service_account.gha[0].email}"
+}
+
+resource "google_project_iam_member" "gha_cloudbuild" {
+  count   = var.create_gha ? 1 : 0
+  project = var.project_id
+  role    = "roles/cloudbuild.builds.editor"
+  member  = "serviceAccount:${google_service_account.gha[0].email}"
+}
+
+# Grants the GHA service account access to the app engine service account
+resource "google_service_account_iam_member" "gha_appengine" {
+  count              = var.create_gha ? 1 : 0
+  service_account_id = data.google_app_engine_default_service_account.default.id
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.gha[0].email}"
+}
 
 resource "google_service_account_key" "main" {
   count              = var.use_secretsmanager ? 1 : 0
